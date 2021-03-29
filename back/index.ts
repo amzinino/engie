c207849f1f6f8e2aee928464f4e0c9b865d2d7ed
Abderahman Jamal <amzinino@gmail.com>
2021-03-29 11:47:48 +0200

    feat(api): Add Agencies API

---
+++ back/index.ts
@@ -0,0 +1,51 @@
+import express from'express';
+
+const app = express();
+const PORT = 8000;
+app.get('/api/agencies', (req,res) => res.json({
+  data: [
+    {
+      type: 'agencies',
+      id: 1,
+      attributes: {
+        name: 'agency1',
+        desc: 'first agency',
+      },
+      links: {
+        self: `http://localhost:3000/api/agency/1`,
+      },
+    },
+    {
+      type: 'agencies',
+      id: 2,
+      attributes: {
+        name: 'agency2',
+        desc: 'second agency',
+      },
+      links: {
+        self: `http://localhost:3000/api/agency/2`,
+      },
+    },
+    {
+      type: 'agencies',
+      id: 3,
+      attributes: {
+        name: 'agency3',
+        desc: 'third agency',
+      },
+      links: {
+        self: `http://localhost:3000/api/agency/3`,
+      },
+    },
+  ],  
+  links: {
+    self: "http://example.com/agencies?page[number]=1&page[size]=1",
+    first: "http://example.com/agencies?page[number]=1&page[size]=1",
+    prev: "http://example.com/agencies?page[number]=1&page[size]=1",
+    next: "http://example.com/agencies?page[number]=1&page[size]=1",
+    last: "http://example.com/agencies?page[number]=13&page[size]=1"
+  }
+}));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
